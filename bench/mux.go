d7ed0c563de0956be699b535176019e9ff3a223a
funkygao <funky.gao@gmail.com>
2015-05-29 10:53:55 +0800

    add stats

--- bench/mux.go
+++ bench/mux.go
@@ -8,27 +8,36 @@ import (
 	"os"
 	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 
+	"github.com/funkygao/golib/gofmt"
+	s "github.com/funkygao/golib/server"
 	"github.com/funkygao/yamux"
 )
 
 const (
-	addr = "localhost:1234"
+	addr = "localhost:10123"
+	//addr = "10.77.144.193:10123"
 )
 
-var opts struct {
-	c    int
-	sz   int
-	n    int
-	mode string
-}
+var (
+	opts struct {
+		c    int
+		sz   int
+		n    int
+		mode string
+	}
+
+	bytesR int64
+	bytesW int64
+)
 
 func init() {
 	flag.IntVar(&opts.c, "c", 100, "concurrency")
 	flag.IntVar(&opts.sz, "s", 100, "size of each msg")
-	flag.IntVar(&opts.n, "n", 1000000, "loops count")
-	flag.StringVar(&opts.mode, "m", "server", "client or server mode")
+	flag.IntVar(&opts.n, "n", 50000, "loops count")
+	flag.StringVar(&opts.mode, "m", "s", "<c|s> mode, c for client and s for server mode")
 
 	flag.Parse()
 }
@@ -41,17 +50,28 @@ func dieIfError(err error) {
 
 func main() {
 	switch opts.mode {
-	case "server":
+	case "s":
 		server()
 
-	case "client":
+	case "c":
 		client()
 
 	default:
-		flag.PrintDefaults()
-		os.Exit(0)
+		flag.Usage()
+		os.Exit(1)
 	}
 
+}
+
+func stats() {
+	tick := time.NewTicker(time.Second * 2)
+	defer tick.Stop()
+
+	for _ = range tick.C {
+		r := atomic.LoadInt64(&bytesR)
+		w := atomic.LoadInt64(&bytesW)
+		fmt.Printf("r:%10s w:%10s\n", gofmt.ByteSize(r), gofmt.ByteSize(w))
+	}
 }
 
 func client() {
@@ -60,25 +80,25 @@ func client() {
 	conn, err := net.Dial("tcp", addr)
 	dieIfError(err)
 
+	go stats()
+
 	session, err := yamux.Client(conn, nil)
 	dieIfError(err)
+	fmt.Printf("session created for %s\n", addr)
 	for i := 0; i < opts.c; i++ {
 		wg.Add(1)
-		go func() {
+		go func(seq int) {
 			defer wg.Done()
 
 			msg := []byte(strings.Repeat("X", opts.sz))
 			stream, err := session.OpenStream()
 			dieIfError(err)
 			for i := 0; i < opts.n; i++ {
-				_, err := stream.Write(msg)
+				n, err := stream.Write(msg)
 				dieIfError(err)
-
-				if i%(opts.n/100) == 1 {
-					println(i)
-				}
+				atomic.AddInt64(&bytesW, int64(n))
 			}
-		}()
+		}(i)
 	}
 
 	wg.Wait()
@@ -86,6 +106,9 @@ func client() {
 }
 
 func server() {
+	go s.RunSysStats(time.Now(), time.Second*9)
+	go stats()
+
 	l, err := net.Listen("tcp", addr)
 	dieIfError(err)
 	fmt.Printf("listen on %s\n", addr)
@@ -102,11 +125,11 @@ func server() {
 func handleConn(conn net.Conn) {
 	session, err := yamux.Server(conn, nil)
 	dieIfError(err)
+	fmt.Printf("session created for %s\n", conn.RemoteAddr())
 
 	for {
 		stream, err := session.AcceptStream()
 		if err == io.EOF {
-			stream.Close()
 			break
 		}
 		dieIfError(err)
@@ -119,10 +142,11 @@ func handleConn(conn net.Conn) {
 func handleStream(st *yamux.Stream) {
 	b := make([]byte, opts.sz)
 	for {
-		_, err := st.Read(b)
-		if err != nil {
-			fmt.Println(err.Error())
-			break
+		n, err := st.Read(b)
+		if err == io.EOF {
+			return
 		}
+		dieIfError(err)
+		atomic.AddInt64(&bytesR, int64(n))
 	}
 }
