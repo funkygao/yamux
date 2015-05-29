a02d813a5e1b1492e114f723edaa548c74531615
funkygao <funky.gao@gmail.com>
2015-05-29 10:58:01 +0800

    misc

---
+++ bench/report.md
@@ -0,0 +1,3 @@
+        free    virt    res     cpu     net     cs      interrupt   time
+sock                                    30                          1m14.762873999s
+mux                                                                 1m50.469296776s
--- bench/sock.go
+++ bench/sock.go
@@ -11,20 +11,26 @@ import (
 )
 
 const (
-	addr = "localhost:1234"
+	addr = "localhost:10123"
+	//addr = "10.77.144.193:10123"
 )
 
-var opts struct {
-	c    int
-	sz   int
-	n    int
-	mode string
-}
+var (
+	opts struct {
+		c    int
+		sz   int
+		n    int
+		mode string
+	}
+
+	bytesR int64
+	bytesW int64
+)
 
 func init() {
 	flag.IntVar(&opts.c, "c", 100, "concurrency")
 	flag.IntVar(&opts.sz, "s", 100, "size of each msg")
-	flag.IntVar(&opts.n, "n", 1000000, "loops count")
+	flag.IntVar(&opts.n, "n", 100000, "loops count")
 	flag.StringVar(&opts.mode, "m", "server", "client or server mode")
 
 	flag.Parse()
@@ -45,7 +51,7 @@ func main() {
 		client()
 
 	default:
-		flag.PrintDefaults()
+		flag.Usage()
 		os.Exit(0)
 	}
 
@@ -56,18 +62,20 @@ func client() {
 	var wg sync.WaitGroup
 	for i := 0; i < opts.c; i++ {
 		wg.Add(1)
-		go func() {
+		go func(i int) {
 			defer wg.Done()
 
+			println("connecting", addr)
 			conn, err := net.Dial("tcp", addr)
 			dieIfError(err)
 
 			msg := []byte(strings.Repeat("X", opts.sz))
-			for i := 0; i < opts.n; i++ {
+			for j := 0; j < opts.n; j++ {
 				_, err := conn.Write(msg)
+				//println(i, j)
 				dieIfError(err)
 			}
-		}()
+		}(i)
 	}
 
 	wg.Wait()
@@ -94,6 +102,7 @@ func handleConn(conn net.Conn) {
 	for {
 		b = b[:]
 		_, err := conn.Read(b)
+		//println(string(b[:n]))
 		dieIfError(err)
 	}
 
